cfe92cdb2e68ebfa6902d374f6afb03997bf0e23
Tiago Chacon <tiagochacon80@gmail.com>
2023-01-09 10:43:05 -0500

    Adding a new collaborator/user to the registry

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
-const Formulario = () => {
+const Formulario = (props) => {
        
     const times = [
         'Programaçao',
@@ -22,7 +22,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido')
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
     
     return (
