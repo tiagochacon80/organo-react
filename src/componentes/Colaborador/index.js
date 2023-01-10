7f8e11f452755fd39c2acbee1fb13059e18d311b
Tiago Chacon <tiagochacon80@gmail.com>
2023-01-10 11:37:07 -0500

    Creating employee cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/tiagochacon80.png' alt='Tiago Chacon' />
+        </div>
+        <div className='rodape'>
+            <h4>Tiago Chacon</h4>
+            <h5>Instrutor</h5>
+        </div>
+    </div>
+
+    )
+}
+
+export default Colaborador
